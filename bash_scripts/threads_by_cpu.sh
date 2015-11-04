4ba21df58da38d1e29b533ce91e18979b7582840
Matt Herrick <matt.herrick@rackspace.com>
2015-11-04 01:11:46 -0600

    rewrote threads_by_cpu, improved output, simplified and corrected some commands

--- bash_scripts/threads_by_cpu.sh
+++ bash_scripts/threads_by_cpu.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-if ps -eo pcpu,pid,args | sort -k 1 -n -r | tr -s " " | head -n 1 | grep -q java;
-	then
-		echo "These are the thread IDs of the Java threads using the CPU, and the amount of CPU they're using:"
-		#Get the ps output for the Java process, then print the thread ID and CPU utilization, excluding any with 0% cpu
-		ps -eLo pid,lwp,nlwp,ruser,pcpu,stime,etime | grep `ps -eo pcpu,pid | sort -k 1 -n -r | tr -s " " | sed 's/^ *//' | head -n 1 | cut -d " " -f 2` | tr -s " " | sort -k 5 -n -r | awk '{print $2, $5}' | grep -v " 0.0"
-		printf "\n\nHere are the converted Hex IDs of the threads using CPU, sorted by cpu utilization. Find them in the thread dump below as \"nid\" to identify problematic processes: \n"
-		#create an array of thread IDs in decimal
-		topthreads=$(ps -eLo pid,lwp,nlwp,ruser,pcpu,stime,etime | grep `ps -eo pcpu,pid | sort -n -k 1 -r | sed 's/^ *//' | tr -s " " | head -n 1 | cut -d " " -f 2` | tr -s " " | sort -n -k 5 -r | grep -v " 0.0"| awk '{print $2}' )
-		#Iterate through the array and output the value as hex.
-		printf '%x\n' ${topthreads[@]}
-		# get the path to jstack, (assumes it's in the bin folder of the JDK that launched the process), the pid of the java process with the highest utilization, and the user it's running as.
-		javapid=$(ps -eo pcpu,pid | sort -k 1 -n -r | tr -s " " | sed 's/^ *//' | head -n 1 | cut -d " " -f 2)
-		javabin=$(ps -eo pid,args | grep ${javapid} | grep -v grep | tr -s " " | sed 's/^ *//' | cut -d " " -f 2 | rev | cut -d "/" -f2- | rev)
-		javauser=$(ps -eo pid,user | grep ${javapid} | grep -v grep | sed 's/^ *//' | cut -d " " -f 2)
-		printf "\n Dumping PID ${javapid} using jstack in ${javabin} as user ${javauser} \n\n"
-		su - ${javauser} -c "${javabin}/jstack -l ${javapid}"
-	else
-		topproc=$(ps -eo pcpu,pid,args | sort -k 1 -n -r | tr -s " " | sed 's/^ *//' | head -n 1 )
-		printf "top utilization process is \n ${topproc} \n"
+
+#ps with no headers and the cpu, pid, and args fields, trim extra spaces, trim leading spaces, sort numerically reverse, cut to the appropriate field, show only the first line.
+topprocpid=`ps --no-headers -eo pcpu,pid,args | tr -s " " | sed 's/^ *//g' |  sort -n -r | cut -d' ' -f2 | head -1`
+topprocname=`ps --no-headers -eo pcpu,pid,args | tr -s " " | sed 's/^ *//g' |  sort -n -r | cut -d' ' -f3 | head -1`
+if echo $topprocname | grep -q java;
+        then
+                echo "Top proc is Java, and it's pid is $topprocpid"
+                # Store the lwps in an array, store the cpu utilization in an array, and then convert the lwps to hex and store the new values in an array
+                lwps=($(ps -eLo pid,lwp,nlwp,ruser,pcpu,stime,etime | grep $topprocpid | tr -s " " | sed 's/^ *//g' | sort -k 5 -n -r | awk '{print $2, $5}' | grep -v "0.0" | awk '{print $1}'))
+                cpu=($(ps -eLo pid,lwp,nlwp,ruser,pcpu,stime,etime | grep $topprocpid | tr -s " " | sed 's/^ *//g' | sort -k 5 -n -r | awk '{print $2, $5}' | grep -v "0.0" | awk '{print $2}'))
+                lwphex=($(printf '%x\n' ${lwps[@]}))
+                i=0
+                #no associative arrays in bash 3, so we will iterate through the hex and cpu arrays in unison.
+                while [ $i -lt ${#lwphex[*]} ]; do
+                        echo Thread NID ${lwphex[$i]} is using ${cpu[$i]}% CPU
+                        i=$(( $i + 1));
+                        done
+                #Try to find jstack from the process arguments and take a thread dump.  This is a best-effort kind of deal, obviously won't work in implementations that don't have jstack, like Websphere.
+                javabin=$(ps -eo pid,args | grep ${topprocpid} | grep -v grep | tr -s " " | sed 's/^ *//' | cut -d " " -f 2 | rev | cut -d "/" -f2- | rev)
+                javauser=$(ps -eo pid,user | grep ${topprocpid} | grep -v grep | sed 's/^ *//' | cut -d " " -f 2)
+                printf "\n Attempting to dump PID ${topprocpid} using jstack in ${javabin} as user ${javauser} \n\n"
+                su - ${javauser} -c "${javabin}/jstack -l ${topprocpid}"
+        else
+                echo "Top process is not Java, it is $topprocname"
 fi
