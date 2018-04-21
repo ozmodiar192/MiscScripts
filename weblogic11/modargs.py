6c41883a00e36ee89918b7a47bbd7b7cfb21f830
Matt Herrick <matt.herrick@rackspace.com>
2018-04-21 10:42:20 -0500

    Re-doing comments, converting jvm args to a variable

--- weblogic11/modargs.py
+++ weblogic11/modargs.py
@@ -1,6 +1,7 @@
 import re
 from sets import Set
-# Variables with dummy values.
+
+# Variables with dummy values that you should update. I assumed you're using user and key files for authentication.
 domain_name='mydomain'
 java_home='/path/to/java/home'
 middleware_home='/opt/app/weblogic'
@@ -11,22 +12,24 @@ weblogic_template=weblogic_home + '/common/templates/wls/wls.jar'
 admin_server_url = 't3://127.0.0.1:9913'
 user_file = "/path/to/userfile"
 key_file = "/path/to/keyfile"
+#This list of arguments is for demonstration and testing only, some of these I just made up.  This will break if you use it!
+new_arguments = "-Xms8192m -Xmx8g -XX:MaxPermSize=2048m -Dweblogic.diagnostics.DisableDiagnosticRuntimeControlService=true -DSomeargument -Dweblogic.debug.DebugClusterFragments=true -Dweblogic.debug.DebugClusterSomethingElse=true"
 
-#This function takes a list of JVM arguments and converts it to a dictionary of key,value pairs with the dict key being the argument, and the value being a complete copy of the argument broken into key, separator, value
+#This function takes a list of JVM arguments and converts it to a dictionary of key,value pairs with the dict key being the jvm argument name, and the value being a complete copy of the argument broken into key, separator, value
 def argdict ( arglist ):
     parsedargdict = {}
     for arg in arglist:
-        # Check for args delimited by :
+        # Check for args delimited by :.  We do this before = because I've found some args that have multiple trailing ='s and let me tell you, that ain't pretty.
         if ":" in arg:
-            #XX options have trailing colons always so that won't do at all.
+            #-XX options have trailing colons, so that won't do at all.
             if "-XX" in arg:
-                # If it's an XX with an =, we'll split at the =, otherwise we'll just add it with no value.
+                # If it's an XX with an =, we'll split at the =, otherwise we'll just add it with no value and pray like hell that it works.
                 if "=" in arg:
                     key,value=arg.split("=")
                     parsedargdict[key] = (key,'=',value)
                 else:
                     parsedargdict[arg] = None
-            # Not an -XX so we can split at : safely. hopefully. You just don't know for sure until you try.
+            # Not an -XX so we can split at : safely. Hopefully. 
             else:
                 key,value=arg.split(":")
                 parsedargdict[key] = (key,':',value)
@@ -43,10 +46,9 @@ def argdict ( arglist ):
         # I'm out of ideas so we'll just add the arg with a key of "none".
         else:
             parsedargdict[arg] = None
-    # Iterate over the current args.  If they exist in the new arg dictionary, add the new arg value.
     return parsedargdict
 
-#this function takes a key value pair and determines if it's a none value.  Returns the key if the value is none, otherwise joins the values.
+#this function takes a key value pair and determines if it's got a none value.  Returns the key if the value is none, otherwise joins the values.
 def flagchecker ( key,value ):
     if value is None:
         argtouse = key
@@ -59,17 +61,18 @@ def flagchecker ( key,value ):
 def genargs ( newargs, server ):
     try:
         cd('/Servers/' + currentserver)
+        # Get the arguments from Weblogic with this command.
         currentargs = cmo.getServerStart().getArguments()
-        # Create a list of new and existing JVM args
+        # Create a list of new and existing JVM args by splitting the strings.
         newarglist = newargs.split()
         currentarglist = currentargs.split()
-        # Create some empty dictionaries
+        # Create some empty dictionaries for fun.
         finalargs = []
         argstonuke = []
         # push the new and current args into dictionaries
         newargdict = argdict(newarglist)
         currentargdict = argdict(currentarglist)
-        # Look through the new args, and check if they're also in the current arguments.  If they are, stick them in the final arguments with the updated values from the new args.
+        # Look through the new args, and check if they're also in the current arguments.  If they are, we know they are existing args with updated values.  We'll  stick them in the final arguments with the updated values from the new args. Follow that?
         for nk,nv in newargdict.iteritems():
             if nk in currentargdict:
                 # Check if we have an argument with a key and a value, or just a key.
@@ -81,13 +84,13 @@ def genargs ( newargs, server ):
         for ck,cv in currentargdict.iteritems():
             curargtoadd = flagchecker(ck,cv)
             finalargs.append(curargtoadd)
-        # Final args now has any carried over arguments, and updated arguments.  Now we need to add any args that are totally new.  We'll convert finalargs to a dict, then look through the new args for any that didn't get added during the check for updated values.
+        # Final args now has any carried over arguments, and the arguments with updated values.  Now we need to add any args that are totally new.  We'll convert finalargs to a dict because I like that function, then look through the new args for any that didn't get added during the check for updated values.
         finalargdict = argdict(finalargs)
         for nk,nv in newargdict.iteritems():
             if nk not in finalargdict:
                 missingarg = flagchecker(nk,nv)
                 finalargs.append(missingarg)
-        # Now we'll tell the user what we've done.  If you're the cautionous type you could get user verification before proceeding.
+        # Now we'll tell the user what we've done and hope it's pleasing.  If you're the cautionous type you could get user verification before calling setargs.
         print("You specified these arguments")
         for arg in newargs.split():
             print(arg)
@@ -116,7 +119,7 @@ def setargs ( mgsvr,jvmargs ):
     cmo.setArguments(jvmargs)
     return
  
-# disconnect from  adminserver with a positive note.
+# disconnect from adminserver with a positive note because feelings matter.
 def disconnectFromAdminserver():
         print 'Disconnecting from the Admin Server.  Have a nice day.';
         disconnect();
@@ -124,17 +127,16 @@ def disconnectFromAdminserver():
 connect(userConfigFile=user_file,userKeyFile=key_file,url=admin_server_url)
 edit()
 startEdit()
-# This iterates through all the servers in the environment.  You probably don't want to do this in real life.
+# This iterates through all the servers in the environment.  You probably don't want to do this in real life unless your environment is highly homogenous.
 try:
     serverNames = cmo.getServers()
     for server in serverNames:
         currentserver=server.getName()
-        # Skip the AdminServer
+        # Skip the AdminServer because come on.
         if currentserver == 'AdminServer':
             print('skipping Admin Server')
         else:
-            #This list of arguments is for demonstration only, some of these I just made up.  This will break if you use it.
-            finalargstring = genargs("-Xms8192m -Xmx8192m -XX:MaxPermSize=2048m -Dweblogic.diagnostics.DisableDiagnosticRuntimeControlService=true -DSomeargument -Dweblogic.debug.DebugClusterFragments=true -Dweblogic.debug.DebugClusterSomethingElse=true", currentserver)
+            finalargstring = genargs(new_arguments,currentserver)
             setargs(currentserver, finalargstring)
 except:
     print("ERROR")
